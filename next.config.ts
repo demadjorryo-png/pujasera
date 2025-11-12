271a374c9b3f1363888f2d38837c37b6d9dba413
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-11-12 10:25:16 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://firebas

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
